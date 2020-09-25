0ef49e8a94fb8459c6f6bbb0962f06295aba34f2
seungho <50465901+csh9066@users.noreply.github.com>
2020-09-25 23:24:09 +0900

    Update Room Component

--- front/src/components/Room.tsx
+++ front/src/components/Room.tsx
@@ -1,17 +1,22 @@
-import { Socket } from 'dgram';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
+
+type Payload = {
+	caller: string;
+	calle: string;
+	sdp: RTCSessionDescriptionInit;
+};
+
 type RoomProps = {};
 
 function Room(props: RoomProps) {
 	const localVideo = useRef<HTMLVideoElement | null>(null);
-	const remoteVideo = useRef<HTMLVideoElement | null>(null);
 	const localStream = useRef<MediaStream | null>(null);
-	const remoteStream = useRef<MediaStream | null>(null);
-	const peer = useRef<RTCPeerConnection | null>(null);
-	const sokcet = useRef<SocketIOClient.Socket | null>(null);
-	const remoteUser = useRef<string | null>(null);
+	// peersRef의 id는 caller는 callee 아이디, callee는 caller 아이디
+	const peersRef = useRef<{ id: string; peer: RTCPeerConnection }[]>([]);
+	const [peers, setPeers] = useState<RTCPeerConnection[]>([]);
+	const socket = useRef<SocketIOClient.Socket | null>(null);
 
 	const { roomID } = useParams<{ roomID: string }>();
 
@@ -24,66 +29,86 @@ function Room(props: RoomProps) {
 			});
 			localVideo.current!.srcObject = mediaStream;
 			localStream.current = mediaStream;
-
 			// socket 서버 접속
-			sokcet.current = io.connect('http://localhost:3005/');
-			sokcet.current.emit('join room', roomID);
+			socket.current = io.connect('http://localhost:3005/');
+			socket.current.emit('join room', roomID);
 
-			// 다른 유저가 접속 하면
-			sokcet.current.on('other user', async (userID: string) => {
-				// //create peerConnection
-				peer.current = new RTCPeerConnection();
+			socket.current.on('ohter users', (users: string[]) => {
+				users.forEach(async (user) => {
+					const peer = await makeCaller(user);
+					peersRef.current.push({ id: user, peer });
 
-				peer.current.onicecandidate = (e) => {
-					if (e.candidate) {
-						const payload: { target: string; candidate: RTCIceCandidate } = {
-							target: remoteUser.current!,
-							candidate: e.candidate,
-						};
-						sokcet.current?.emit('ice-candidate', payload);
-					}
-				};
-
-				peer.current.addEventListener('track', (e: RTCTrackEvent) => {
-					remoteVideo.current!.srcObject = e.streams[0];
+					setPeers((prevState) => [...prevState, peer]);
 				});
-
-				peer.current.addEventListener('negotiationneeded', async (e) => {
-					const offer = await peer.current!.createOffer();
-					peer.current?.setLocalDescription(offer);
-					const payload: {
-						target: string;
-						caller: string;
-						sdp: RTCSessionDescription | null;
-					} = {
-						target: userID,
-						caller: sokcet.current!.id,
-						sdp: peer.current!.localDescription,
-					};
-					sokcet.current?.emit('offer', payload);
-				});
-
-				localStream.current?.getVideoTracks().forEach((track) => {
-					peer.current?.addTrack(track);
-				});
-
-				remoteUser.current = userID;
 			});
 
-			sokcet.current.on('user join', (userID: string) => {
-				remoteUser.current = userID;
+			socket.current.on('new-ice-candidate', async () => {});
+
+			// 콜리는 offer만 받음
+			socket.current.on('offer', async ({ calle, sdp, caller }: Payload) => {
+				const peer = await makeCallee(caller);
+				peersRef.current.push({ id: caller, peer });
+
+				setPeers((prevState) => [...prevState, peer]);
+
+				await peer.setRemoteDescription(new RTCSessionDescription(sdp));
+
+				socket.current?.emit('answer', {
+					caller,
+					calle,
+					sdp: peer.localDescription,
+				});
 			});
 
-			sokcet.current.on('offer', () => {});
-			sokcet.current.on('answer', () => {});
-			sokcet.current.on('ice-candidate', () => {});
+			// 콜러는 answer만 받음
+			socket.current.on('answer', async ({ calle, sdp }: Payload) => {
+				const peerObj = peersRef.current.find((peer) => peer.id === calle);
+				if (peerObj) {
+					const peer = peerObj.peer;
+					await peer.setRemoteDescription(new RTCSessionDescription(sdp));
+				}
+			});
 		})();
 	}, []);
+
+	async function makeCaller(callee: string) {
+		const peer: RTCPeerConnection = new RTCPeerConnection();
+		const offer = await peer.createOffer();
+		await peer.setLocalDescription(offer);
+		socket.current?.emit('offer', {
+			// 콜리는 오퍼를 받는 사람 콜러는 나
+			caller: socket.current.id,
+			callee,
+			sdp: offer,
+		});
+		peer.onicecandidate = (e) => {
+			if (e.candidate) {
+				// socket.emit('new-ice-candidate',)
+			}
+		};
+		return peer;
+	}
+
+	async function makeCallee(caller: string) {
+		const peer: RTCPeerConnection = new RTCPeerConnection();
+		const answer = await peer.createAnswer();
+		await peer.setLocalDescription(answer);
+		socket.current?.emit('answer', {
+			caller,
+			calle: socket.current.id,
+			sdp: answer,
+		});
+		return peer;
+	}
 
 	return (
 		<div>
 			<video autoPlay ref={localVideo} />
-			<video autoPlay ref={remoteVideo} />
+			{/* {
+				peers.map((peer) => {
+					<video autoPlay ref={localVideo} />
+				})
+			} */}
 		</div>
 	);
 }
